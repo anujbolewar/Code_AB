04c9daea3c2f87ff30c1a083878019702e5a14c1
Anuj <bolewara@gmail.com>
2025-09-17 20:25:26 -0700

    Add authentication setup and prisma integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 
@@ -6,8 +5,8 @@ import { db } from "@/lib/db";
 export default async function Home() {
 const user = db.user.
   return (
-  <div>
+  <>
 <Button className="bg-blue-500 hover:bg-blue-700 ">Get Started</Button>
-  </div>
+  </>
   );
 }
